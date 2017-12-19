a2e2b47deba182ca5832249f8d834a1d7f83d4bb
root <root@priyalp622.mylabserver.com>
2017-12-19 05:37:10 +0000

    add spring boot

---
+++ src/main/java/com/priyal/calci/CalciApplication.java
@@ -0,0 +1,12 @@
+package com.priyal.calci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalciApplication.class, args);
+	}
+}
